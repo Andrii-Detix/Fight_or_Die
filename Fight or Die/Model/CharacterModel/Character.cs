a89a93313e0d2ab0b9e9356c1142fa343f534054
Andrii-Detix <aivancisin6@gmail.com>
2024-05-25 22:25:50 +0300

    Update Character.cs

--- Fight or Die/Model/CharacterModel/Character.cs
+++ Fight or Die/Model/CharacterModel/Character.cs
@@ -1,4 +1,6 @@
-﻿using Fight_or_Die.Abstractions;
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+using Fight_or_Die.Abstractions;
 using Fight_or_Die.Model.HealthModel;
 
 namespace Fight_or_Die.Model.CharacterModel;
@@ -8,12 +10,30 @@ public class Character : IDamagable
     public Character(int maxHealth)
     {
         Health = new Health(maxHealth);
+        Enable();
     }
-    
+
+    public event Action<Character>? Died;
     public Health Health { get; }
-    
+
     public void TakeDamage(int points)
     {
         Health.AddHealth(-points);
+    }
+
+    private void OnDied()
+    {
+        Died?.Invoke(this);
+        Disable();
+    }
+
+    private void Enable()
+    {
+        Health.Died += OnDied;
+    }
+
+    private void Disable()
+    {
+        Health.Died -= OnDied;
     }
 }
