0eba3b339266532e102131c0ab85b549ccd3b75c
Andrii-Detix <aivancisin6@gmail.com>
2024-05-25 20:20:23 +0300

    Add Health.cs

---
+++ Fight or Die/Model/Health/Health.cs
@@ -0,0 +1,36 @@
+﻿namespace Fight_or_Die.Model.Health;
+
+public class Health
+{
+    public Health(int maxHealth)
+    {
+        _maxHealth = maxHealth;
+        _health = _maxHealth;
+    }
+
+    private readonly int _maxHealth;
+    private readonly int _minHealth = 0;
+    private int _health;
+    
+    public event Action Died;
+    public event Action HealthChanged;
+
+    public void AddHealth(int points)
+    {
+        if(points == 0)
+            return;
+
+        int newHealth = _health + points;
+
+        if (newHealth > _maxHealth)
+            _health = _maxHealth;
+
+        if (newHealth <= _minHealth)
+        {
+            _health = _minHealth;
+            Died?.Invoke();
+        }
+        
+        HealthChanged?.Invoke();
+    }
+}
