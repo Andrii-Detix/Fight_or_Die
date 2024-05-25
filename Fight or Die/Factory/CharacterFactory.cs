4cb07cd5a8d6f1d268b20c595d1ac860d5b87005
Andrii-Detix <aivancisin6@gmail.com>
2024-05-26 01:38:45 +0300

    Add factory and character config

---
+++ Fight or Die/Factory/CharacterFactory.cs
@@ -0,0 +1,19 @@
+﻿using Fight_or_Die.Configs;
+using Fight_or_Die.Model.CharacterModel;
+
+namespace Fight_or_Die.Factory;
+
+public class CharacterFactory : IFactory<Character>
+{
+    public CharacterFactory(CharacterConfig config)
+    {
+        _config = config;
+    }
+
+    private readonly CharacterConfig _config;
+
+    public Character Create()
+    {
+        return new Character(_config.MaxHalth, _config.Size);
+    }
+}
---
+++ Fight or Die/Factory/IFactory.cs
@@ -0,0 +1,6 @@
+﻿namespace Fight_or_Die.Factory;
+
+public interface IFactory<T>
+{
+    T Create();
+}
