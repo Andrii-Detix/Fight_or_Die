41498962daaa8c94016bf4ab41a0d98770e508d2
Andrii-Detix <aivancisin6@gmail.com>
2024-05-26 01:39:06 +0300

    Add map

---
+++ Fight or Die/Model/PlateModel/Plate.cs
@@ -0,0 +1,15 @@
+﻿using Fight_or_Die.GeometryElements;
+
+namespace Fight_or_Die.Model.PlateModel;
+
+public class Plate
+{
+    public Plate(Vector position, Size size)
+    {
+        Position = position;
+        Size = size;
+    }
+
+    public readonly Vector Position;
+    public readonly Size Size;
+}
