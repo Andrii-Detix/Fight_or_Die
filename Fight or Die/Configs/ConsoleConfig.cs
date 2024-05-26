796a489957ad7be5f06163b0418e8e1a4067a1c6
Andrii-Detix <aivancisin6@gmail.com>
2024-05-26 18:03:14 +0300

    Add ConsoleConfig.cs and Map.Builder

---
+++ Fight or Die/Configs/ConsoleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Fight_or_Die.Configs;
+
+public class ConsoleConfig
+{
+    public ConsoleConfig()
+    {
+    minUserX = OutLineThick;
+    minUserY = OutLineThick;
+    maxUserX = ConsoleWidth - OutLineThick - Displacement;
+    maxUserY = ConsoleHeight - OutLineThick - Displacement;
+    }
+    public readonly int ConsoleWidth = 100;
+    public readonly int ConsoleHeight = 30;
+    public readonly int OutLineThick = 1;
+    public readonly int Displacement = 1;
+    public readonly int minUserX;
+    public readonly int minUserY;
+    public readonly int maxUserX;
+    public readonly int maxUserY;
+}
---
+++ Fight or Die/Model/MapModel/MapBuilder.cs
@@ -0,0 +1,47 @@
+﻿using Fight_or_Die.Configs;
+using Fight_or_Die.GeometryElements;
+using Fight_or_Die.Model.PlateModel;
+
+namespace Fight_or_Die.Model.MapModel;
+
+public class MapBuilder
+{
+    public MapBuilder(ConsoleConfig config)
+    {
+        _config = config;
+    }
+
+    private readonly ConsoleConfig _config;
+
+    public Map Create()
+    {
+        List<Plate> plates = new List<Plate>();
+        
+        Size plateSize = new Size(8, 1);
+        
+        Vector rowDisplacment = new Vector(plateSize.Width * 2, 0);
+        Vector vertDisplacment = new Vector(0, 5);
+        
+        bool isIndentRow = false;
+
+        Vector position = new Vector(_config.maxUserY, _config.minUserX);
+        
+        while (position.Y - plateSize.Height >= _config.minUserY)
+        {
+            position = isIndentRow
+                ? new Vector(plateSize.Width, position.Y)
+                : (new Vector(_config.minUserX, position.Y));
+            
+            while (position.X + plateSize.Width <= _config.maxUserX)
+            {
+                plates.Add(new Plate(position, plateSize));
+                position += rowDisplacment;
+            }
+
+            position -= vertDisplacment;
+            isIndentRow = !isIndentRow;
+        }
+
+        return new Map(plates);
+    }
+}
