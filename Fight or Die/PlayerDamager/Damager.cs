852d07053b978585ed52035c574ac7b9ae003c78
Andrii-Detix <aivancisin6@gmail.com>
2024-05-27 23:02:59 +0300

    Add Damager.cs

---
+++ Fight or Die/PlayerDamager/Damager.cs
@@ -0,0 +1,33 @@
+﻿using Fight_or_Die.Abstractions;
+using Fight_or_Die.Configs;
+using Fight_or_Die.Model.CharacterModel;
+
+namespace Fight_or_Die.PlayerDamager;
+
+public class Damager
+{
+    public Damager(IDamagable player, DamagerConfig config)
+    {
+        _player = player;
+        _config = config;
+    }
+    
+    private readonly IDamagable _player;
+    private readonly DamagerConfig _config;
+    private int _damageTimer = 0;
+
+    public void Udpate()
+    {
+        MakeDamage();
+    }
+    private void MakeDamage()
+    {
+        _damageTimer++;
+        
+        if (_damageTimer ==_config.DamageInterval)
+        {
+            _player.TakeDamage(_config.Damage);
+            _damageTimer = 0;
+        }
+    }
+}
