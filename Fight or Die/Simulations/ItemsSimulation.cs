4efb3bac34ad2dc1d54f68fe5fd8022d469423c4
Andrii-Detix <aivancisin6@gmail.com>
2024-05-27 22:13:12 +0300

    Update ItemSimulation

--- Fight or Die/Simulations/ItemsSimulation.cs
+++ Fight or Die/Simulations/ItemsSimulation.cs
@@ -5,6 +5,8 @@ namespace Fight_or_Die.Simulations;
 
 public class ItemsSimulation : AbstractSimulation<Item>
 {
+    public event Action<Item>? Start;
+    public event Action<Item>? End;
     public override void Simulate(Item item)
     {
         if(_entities.Contains(item))
@@ -12,6 +14,7 @@ public class ItemsSimulation : AbstractSimulation<Item>
         
         OnEnable(item);
         Add(item);
+        Start?.Invoke(item);
     }
 
     protected override void Stop(Item item)
@@ -21,15 +24,16 @@ public class ItemsSimulation : AbstractSimulation<Item>
         
         OnDisable(item);
         Remove(item);
+        End?.Invoke(item);
     }
 
     private void OnEnable(Item item)
     {
-        
+        item.Destroyed += Stop;
     }
 
     private void OnDisable(Item item)
     {
-        
+        item.Destroyed -= Stop;
     }
 }
